b383a758dd9f9c4b1dfe008a84b300452c0022cd
sungminyun1 <smy07207@gmail.com>
2023-03-28 23:59:41 +0900

    2743

---
+++ src/bronze/P2743.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+}
