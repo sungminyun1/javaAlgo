9723fada5d68036290fcd061e245d04d83b4bf41
sungminyun1 <smy07207@gmail.com>
2023-04-06 00:39:22 +0900

    1152

---
+++ src/bronze/P1152.java
@@ -0,0 +1,18 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class P1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(input," ");
+        System.out.println(st.countTokens());
+    }
+}
