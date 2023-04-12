a88a7e196af5698dda4ec411d253b8b6e988baae
sungminyun1 <smy07207@gmail.com>
2023-04-13 01:15:47 +0900

    1238

--- src/gold/P1238.java
+++ src/gold/P1238.java
@@ -3,84 +3,85 @@ package gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class P1238 {
-    private static int[][] MAP;
-    private static int N;
-    private static int M;
-    private static int X;
-    private static int MAX = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    private static int N,M,X,MAX;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    private static List<List<Node>> graph, reverseGraph;
+    private static int[] dist,reverseDist;
+
+    static class Node implements Comparable<Node>{
+        public int index, distance;
+        public Node(int index, int distance){
+            this.index = index;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            return distance - node.distance;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
 
-        MAP = new int[N + 1][N + 1];
-        for(int i =0; i<N+1; i++){
-            Arrays.fill(MAP[i], Integer.MAX_VALUE);
+        graph = new ArrayList<>();
+        reverseGraph = new ArrayList<>();
+        for(int i =0; i<=N; i++){
+            graph.add(new ArrayList<>());
+            reverseGraph.add(new ArrayList<>());
         }
+        dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        reverseDist = new int[N + 1];
+        Arrays.fill(reverseDist, Integer.MAX_VALUE);
 
-        for(int i = 0; i<M; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int dest = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            MAP[start][dest] = cost;
+        for(int i =0; i<M; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            graph.get(s).add(new Node(e, d));
+            reverseGraph.get(e).add(new Node(s, d));
         }
-
-        for(int i=1; i<=N; i++){
-            int maxCandi = Dijkstra(i, X) + Dijkstra(X, i);
-            MAX = Math.max(MAX, maxCandi);
+        Di(graph, dist, X);
+        Di(reverseGraph, reverseDist, X);
+        MAX = 0;
+        for(int i=1; i<dist.length; i++){
+            MAX = Math.max(MAX, dist[i] + reverseDist[i]);
         }
-
         System.out.println(MAX);
     }
 
-    public static int Dijkstra(int start, int dest){
-        int[] visited = new int[N+1];
-        int[] shortWay = new int[N+1];
-        for(int i =1; i<=N; i++){
-            shortWay[i] = MAP[start][i];
-        }
-        visited[start] = 1;
-        while(true){
-            if(isDone(visited)){
-                break;
-            }
-            int t_min = Integer.MAX_VALUE;
-            int nextIdx = 0;
-            for(int i=1; i<shortWay.length; i++){
-                if(visited[i] == 0 && shortWay[i] < t_min){
-                    t_min = shortWay[i];
-                    nextIdx = i;
-                }
-            }
+    public static void Di(List<List<Node>> list, int[] dist, int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[N + 1];
+        Arrays.fill(visited, false);
+        pq.add(new Node(start, 0));
+        dist[start] = 0;
 
-            visited[nextIdx] = 1;
-            for(int i =1; i<=N; i++){
-                if(MAP[nextIdx][i] != Integer.MAX_VALUE){
-                    int candi = shortWay[nextIdx] + MAP[nextIdx][i];
-                    if(candi < shortWay[i]){
-                        shortWay[i] = candi;
-                    }
+        while (!pq.isEmpty()) {
+            int idx = pq.poll().index;
+
+            if (visited[idx]) {
+                continue;
+            }
+            visited[idx] = true;
+
+            for (Node next : list.get(idx)) {
+                if (dist[next.index] > dist[idx] + next.distance) {
+                    dist[next.index] = dist[idx] + next.distance;
+                    pq.add(new Node(next.index, dist[next.index]));
                 }
             }
         }
-
-        return shortWay[dest];
-    }
-
-    public static boolean isDone(int[] visited){
-        for(int i =1; i<visited.length; i++){
-            if(visited[i] == 0){
-                return false;
-            }
-        }
-        return true;
     }
 }
