9a05c4fbca142a5cdaea9e5e3f65efa5a623d9be
sungminyun1 <smy07207@gmail.com>
2023-07-03 23:34:21 +0900

    PSVM test

--- src/anything/PSVM.java
+++ src/anything/PSVM.java
@@ -4,15 +4,7 @@ import java.util.*;
 
 public class PSVM {
     public static void main(String[] args) {
-        ArrayList<String> initList = new ArrayList<>();
-        initList.add("hello");
-        Immutable immutable = new Immutable(0,initList);
-
-        List<String> list = immutable.getList();
-        System.out.println(immutable.getList());
-
-        list.add("world");
-        System.out.println(immutable.getList());
-        System.out.println(list);
+        ArrayList<Integer> a = new ArrayList<>();
+        System.out.println(a);
     }
 }
