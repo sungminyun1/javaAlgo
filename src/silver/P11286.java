88c6be0c0aac83b112e74699726de407cdb6b0b9
sungminyun1 <smy07207@gmail.com>
2023-08-08 21:35:27 +0900

    P11286

--- src/silver/P11286.java
+++ src/silver/P11286.java
@@ -4,32 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class P11279 {
+public class P11286 {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    @Deprecated
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         ArrayList<String> ans = new ArrayList<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
         for(int i =0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int val = Integer.parseInt(st.nextToken());
             if(val == 0){
-                if (pq.size() > 0) {
-                    ans.add(""+pq.poll());
-                } else {
-                    ans.add(""+0);
+                if(pq.size() > 0){
+                    ans.add("" + pq.poll().val);
+                }else {
+                    ans.add("" + 0);
                 }
             }else{
-                pq.add(val);
+                pq.add(new Node(val));
             }
         }
 
-        System.out.println(String.join("\n",ans));
+        System.out.println(String.join("\n", ans));
+    }
+
+    public static class Node implements Comparable<Node>{
+        public int val;
+        Node(int val){
+            this.val = val;
+        }
+        @Override
+        public int compareTo(Node o) {
+            if( Math.abs(val) == Math.abs(o.val)){
+                return val - o.val;
+            }
+            return Math.abs(val) - Math.abs(o.val);
+        }
     }
 }
