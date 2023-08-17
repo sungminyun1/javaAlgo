2483a81fae7241a6f319746f1cc514609a060a5b
sungminyun1 <smy07207@gmail.com>
2023-08-17 12:26:09 +0900

    P2437

--- src/gold/P2437.java
+++ src/gold/P2437.java
@@ -4,38 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class P2437 {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static ArrayList<Integer> arr = new ArrayList<>();
+    public static int[] arr;
     public static HashSet<Integer> hs = new HashSet<>();
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
+        arr = new int[N];
         for(int i =0; i<N; i++){
-            arr.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        getCase(0,0);
-        int val = 1;
-        while(true){
-            if(!hs.contains(val)){
-                System.out.println(val);
-                return;
-            }
-            val++;
+        int sum = 0;
+        Arrays.sort(arr);
+
+        for(int i =0; i<arr.length; i++){
+            if(arr[i] > sum +1) break;
+            sum += arr[i];
         }
+        System.out.println(sum+1);
     }
-    static void getCase(int start, int now){
-        if (start >= arr.size()) {
-            return;
-        }
-        for(int i = start; i< arr.size(); i++){
-            int val = now + arr.get(i);
-            hs.add(val);
-            getCase(i+1,val);
-        }
-    }
+
 }
