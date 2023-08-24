6559d31e919d371683c0835d3e2e7513424b19ec
sungminyun1 <smy07207@gmail.com>
2023-08-24 19:15:37 +0900

    P1697

--- src/barkingdog/ch09/P1697.java
+++ src/barkingdog/ch09/P1697.java
@@ -3,6 +3,7 @@ package barkingdog.ch09;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -17,34 +18,25 @@ public class P1697 {
 
         Queue<Integer> nv = new LinkedList<>();
         final int MAX = 200001;
-        boolean[] visited = new boolean[MAX];
-        visited[start] = true;
-        nv.add(start);
 
-        int count = 0;
-        while (!nv.isEmpty()) {
-            int ns = nv.size();
-            for (int i = 0; i < ns; i++) {
-                int now = nv.poll();
-                if(now == dest){
-                    System.out.println(count);
-                    return;
-                }
-                if (now + 1 < MAX && !visited[now + 1]) {
-                    visited[now+1] = true;
-                    nv.add(now + 1);
-                }
-                if (now - 1 > 0 && !visited[now - 1]) {
-                    visited[now-1] = true;
-                    nv.add(now - 1);
-                }
-                if (now *2 < MAX && !visited[now *2]) {
-                    visited[now*2] = true;
-                    nv.add(now *2);
-                }
+        int[] dist = new int[MAX];
+        Arrays.fill(dist, -1);
+        nv.add(start);
+        dist[start] = 0;
+
+        while(!nv.isEmpty()){
+            int now = nv.poll();
+            if(now == dest){
+                System.out.println(dist[now]);
+                return;
             }
-            count++;
+
+            int[] nexts = {now + 1, now - 1, now * 2};
+            for (int next : nexts) {
+                if(next < 0 || next >= MAX || dist[next] >= 0) continue;
+                dist[next] = dist[now] +1;
+                nv.add(next);
+            }
         }
-        System.out.println(count);
     }
 }
