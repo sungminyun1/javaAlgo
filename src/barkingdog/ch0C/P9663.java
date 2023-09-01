02bf73a0b2fe40764b9214ca31799a6b9113f538
sungminyun1 <smy07207@gmail.com>
2023-09-01 13:16:22 +0900

    P9663

--- src/barkingdog/ch0C/P9663.java
+++ src/barkingdog/ch0C/P9663.java
@@ -6,44 +6,38 @@ import java.io.InputStreamReader;
 
 public class P9663 {
     public static int N;
-    public static int[][] board;
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static int ans = 0;
+    public static boolean[] v1 = new boolean[40];
+    public static boolean[] v2 = new boolean[40];
+    public static boolean[] v3 = new boolean[40];
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        board = new int[N][N];
-        run(0, 0, 0);
+        run(0);
         System.out.println(ans);
     }
 
-    public static void run(int level, int r, int c) {
-        if (level == N) {
+    public static void run(int cur){
+        if(cur >= N){
             ans++;
             return;
         }
-        for (int i = r; i < N; i++) {
-            for (int j = c; j < N; j++) {
-                if(board[i][j] == 0 && check(i,j)){
-                    board[i][j] = 1;
-                    run(level + 1, i +1, j + 1);
-                    board[i][j] = 0;
-                }
-            }
+
+        for (int i = 0; i < N; i++) {
+            int c1 = i; // 같은 행 또는 열에 있는거 체크
+            int c2 = cur + i; // 우측 대각선 확인, [0,0] 과 [1,-1], [-1,1] 처럼 우측 대각선에 있다면 두 합이 같음
+            int c3 = cur - i + N - 1; // 좌측 대각선 확인, [0,0], [1,1], [-1,-1] 처럼 좌측 대각선에 있다면 두 차가 같음. 음수가 될수 있기에 N -1 을 더해주어 넉넉하게 계산
+            if(v1[c1] || v2[c2] || v3[c3]) continue;
+            v1[c1] = true;
+            v2[c2] = true;
+            v3[c3] = true;
+            run(cur+1);
+            v1[c1] = false;
+            v2[c2] = false;
+            v3[c3] = false;
         }
     }
 
-    public static boolean check(int r, int c) {
-        int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
-        int[] dy = {0, 0, -1, 1, -1, 1, -1, 1,};
-        for (int i = 0; i < N; i++) {
-            for (int k = 0; k < 8; k++) {
-                int nx = r + dx[k] * i;
-                int ny = c + dy[k] * i;
-                if(nx <0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] == 0) continue;
-                return false;
-            }
-        }
-        return true;
-    }
+
 }
