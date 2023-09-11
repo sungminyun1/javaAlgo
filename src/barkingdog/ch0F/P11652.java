b56452658b789d8db94d004675aee3d736acb7e6
sungminyun1 <smy07207@gmail.com>
2023-09-11 19:26:40 +0900

    _P11652

---
+++ src/barkingdog/ch0F/P11652.java
@@ -0,0 +1,41 @@
+package barkingdog.ch0F;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P11652 {
+    public static int N;
+    public static int[] arr;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int val = Integer.parseInt(br.readLine());
+            arr[i] = val;
+        }
+        Arrays.sort(arr);
+        int maxNum = arr[0];
+        int maxCount = 0;
+
+        int nowNum = arr[0];
+        int nowCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == nowNum){
+                nowCount++;
+            }else{
+                nowNum = arr[i];
+                nowCount = 1;
+            }
+
+            if(maxCount < nowCount){
+                maxNum = arr[i];
+                maxCount = nowCount;
+            }
+        }
+
+        System.out.println(maxNum);
+    }
+}
