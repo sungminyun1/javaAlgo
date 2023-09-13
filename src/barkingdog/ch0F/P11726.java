66a8402f2b4c651b4167ccb56fb3fd1ccd92ddf6
sungminyun1 <smy07207@gmail.com>
2023-09-13 13:30:38 +0900

    P11726

---
+++ src/barkingdog/ch0F/P11726.java
@@ -0,0 +1,23 @@
+package barkingdog.ch0F;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11726 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        if(N < 3){
+            System.out.println(N);
+            return;
+        }
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
