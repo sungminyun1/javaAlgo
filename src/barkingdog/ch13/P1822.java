c27784537d0d316658a43a111f15cca997840089
sungminyun1 <smy07207@gmail.com>
2023-10-25 21:46:37 +0900

    P1822

---
+++ src/barkingdog/ch13/P1822.java
@@ -0,0 +1,41 @@
+package barkingdog.ch13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P1822 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        int[] B = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int count = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : A) {
+            if(Arrays.binarySearch(B,i) < 0){
+                count++;
+                sb.append(i + " ");
+            }
+        }
+
+        System.out.println(count);
+        if(count > 0) System.out.println(sb);
+    }
+}
